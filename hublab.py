522222cb10ad8ee21f04e74d97372be3e535803d
Marcus Shepherd <marcus.shepherd@outlook.com>
2015-03-11 11:17:08 -0400

    documentation

--- hublab.py
+++ hublab.py
@@ -4,8 +4,14 @@ import re
 from pygithub3 import Github
 import gitlab
 
+"""	
+Short Script for iterating over issues in a gitlab project.
+Then creating each issue in a specified repo on GitHub.
+All values with '#' must be filled in by user.
+"""
+
 #gitlab
-gl = gitlab.Gitlab("141.209.28.74", token="1isGAgVpjqresUDkwRNB", verify_ssl=False)
+gl = gitlab.Gitlab("141.209.28.74", token="#", verify_ssl=False)
 
 # github
 auth = dict(login='#', password='#')
@@ -16,10 +22,14 @@ project_ids = {
 	'centraldesk': 62,
 	'sodium': 2,
 }
-issues = gl.getprojectissues(project_ids['urec'], per_page=100) # defaults to 20 results
+issues = gl.getprojectissues(project_ids['urec'], per_page=100) # defaults to 20 results, max is 100
 for i in range(len(issues)):
+	""" 
+	Currently pointing at urec, simply change 'urec' to any other
+	value in project_ids in order to iterate over another project.
+	"""
+	
 	date = gl.getprojectissues(project_ids['urec'])[i]['created_at']
-
 	data = {
 	'title': gl.getprojectissues(project_ids['urec'])[i]['title'],
 	'body': "Date: "+ " ".join(re.split("T|\\.", date)[:2])+"\n"+" Desciption: "+gl.getprojectissues(project_ids['urec'])[i]['description'],
