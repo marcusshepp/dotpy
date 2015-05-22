dd01bb25f110d9aec79c3d59cd1f5f59bed9f7e0
sheph2mj <sheph2mj@cmich.edu>
2015-05-22 16:32:50 -0400

    hacker ranker

--- hackerrank/python/sorting/insertionsortp1.py
+++ hackerrank/python/sorting/insertionsortp1.py
@@ -1,11 +1,13 @@
 def insertionsort(l):
 	value = l[len(l)-1]
 	time = ""
-	for i in l:
+	for i in reversed(l):
 		if i > value:
-			l[l.index(i)] = i
-		else:
-			l[l.index(i)] = value
+			# print "l[l.index(i)]: ", l[l.index(i)]
+			l[l.index(i)+1] = i
+		if i <= value:
+			print "l.index(i)+1: ", l.index(i)+1
+			l[l.index(i)-1] = value
 		time+=str(l)+"\n"
 	return time
 
